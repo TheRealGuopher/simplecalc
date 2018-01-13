3160ee3c10e311e1ecf2ded4022b571a432eb410
TheRealGuopher <jjguo@uw.edu>
2018-01-12 16:22:20 -0800

    Working on all criteria and negative numbers, currently no floating-point numbers

--- SimpleCalc/SimpleCalc/main.swift
+++ SimpleCalc/SimpleCalc/main.swift
@@ -9,27 +9,22 @@
 import Foundation
 
 public class Calculator {
-    public func calculate(_ args: [String]) -> Int {
-        let op = args[args.count - 1]
-        switch op {
-        case "avg":
-            var total: Int = 0
-            for (index, num) in args.enumerated() {
-                if index != args.count - 1 {
-                    total += Int(num)!
-                }
-            }
-            return total / (args.count - 1)
-        case "count":
-            return args.count - 1
-        case "fact":
-            var index: Int = Int(args[0])!
-            var total: Int = 1
-            while index > 0 {
-                total *= index
-                index -= 1
-            }
-            return total
+    public func calculateThree(args: [String], operation: String) -> Int {
+//        if operation == "+" {
+//            return Int(args[0])! + Int(args[2])!
+//        } else if operation == "-" {
+//            return Int(args[0])! - Int(args[2])!
+//        } else if operation == "*" {
+//            return Int(args[0])! * Int(args[2])!
+//        } else if operation == "/" {
+//            return Int(args[0])! / Int(args[2])!
+//        } else if operation == "%" {
+//            let min = Int(args[0])! / Int(args[2])!
+//            return Int(args[0])! - (min * Int(args[2])!)
+//        } else {
+//            return -1
+//        }
+        switch args[1] {
         case "+":
             return Int(args[0])! + Int(args[2])!
         case "-":
@@ -45,7 +40,47 @@ public class Calculator {
             return 1
         }
     }
-    
+
+    public func calculate(_ args: [String]) -> Int {
+        var expressions = [String]()
+        for value in args {
+            expressions.append(value)
+        }
+        if expressions.count == 3 {
+            let operation = args[1]
+            return calculateThree(args: expressions, operation: operation)
+        }
+        let op = args[args.count - 1]
+        switch op {
+        case "avg":
+            if args.count == 1 {
+                return 0
+            }
+            var total: Int = 0
+            for (index, num) in args.enumerated() {
+                if index != args.count - 1 {
+                    total += Int(num)!
+                }
+            }
+            return total / (args.count - 1)
+        case "count":
+            return args.count - 1
+        case "fact":
+            if args.count == 1 {
+                return 0
+            }
+            var index: Int = Int(args[0])!
+            var total: Int = 1
+            while index > 0 {
+                total *= index
+                index -= 1
+            }
+            return total
+        default:
+            return 0
+        }
+    }
+
     public func calculate(_ arg: String) -> Int {
         return calculate( arg.split(separator: " ").map({ substr in String(substr) }) )
     }
@@ -56,7 +91,7 @@ public class Calculator {
 print("UW Calculator v1")
 print("Enter an expression separated by returns:")
 let first = readLine()!
-//let operation = readLine()!
+let operation = readLine()!
 let second = readLine()!
-print(Calculator().calculate([first, second]))
+print(Calculator().calculate([first, operation, second]))
 
